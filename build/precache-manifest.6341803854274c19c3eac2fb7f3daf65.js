ce9c28dca639e54fe97f29bee71d804831d20010
Felipe Morato <felipecesar.morato@gmail.com>
2020-05-18 19:25:57 -0300

    segunda aula react

---
+++ build/precache-manifest.6341803854274c19c3eac2fb7f3daf65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b57500fba4cb980893e29330d3170da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46d5d5a387bf8b69df85",
+    "url": "/static/css/main.f7d8162b.chunk.css"
+  },
+  {
+    "revision": "b66a33c610e9422d6aad",
+    "url": "/static/js/2.2571301e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2571301e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d5d5a387bf8b69df85",
+    "url": "/static/js/main.db9f91c5.chunk.js"
+  },
+  {
+    "revision": "2eef4fcd0e858fec49d3",
+    "url": "/static/js/runtime-main.35890d13.js"
+  }
+]);
